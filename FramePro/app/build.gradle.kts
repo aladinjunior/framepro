aa3e3318272d61861fedcf344698c003df280ece
Aladin Júnior <aladinjr013@icloud.com>
2023-11-08 15:16:18 -0300

    dependecies updated

--- FramePro/app/build.gradle.kts
+++ FramePro/app/build.gradle.kts
@@ -33,14 +33,24 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.0-alpha02")
+
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
